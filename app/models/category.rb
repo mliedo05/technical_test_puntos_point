5d75401ba24f6d2c21f70d18a2c1d49d712a1d36
mliedo05 <79027650+mliedo05@users.noreply.github.com>
2025-04-11 10:51:34 -0400

    Revert "Feature/create purchase model"

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,3 @@
 class Category < ApplicationRecord
   belongs_to :admin
-  has_many :product_categories
-  has_many :products, through: :product_categories
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
 class Product < ApplicationRecord
   belongs_to :admin
-  has_many :product_categories
-  has_many :categories, through: :product_categories
 end
