5361f1308122af9a88e30eacfae01fbd81baabc8
mliedo05 <m.liendo@wapii.com>
2025-04-11 11:17:41 -0400

    fixed last merged

---
+++ db/migrate/20250411151703_create_product_categories.rb
@@ -0,0 +1,8 @@
+class CreateProductCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_categories do |t|
+      t.references :product, foreign_key: true
+      t.references :category, foreign_key: true
+    end
+  end
+end
